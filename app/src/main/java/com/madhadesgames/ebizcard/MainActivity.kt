cf5098cb2a76fd30b570c12828a413ea51eceaf6
MadHadesGames <madhadesgames@gmail.com>
2022-11-21 11:14:39 +0100

    Adding Infos and Button
    switch to ubuntu 22.10 + adding infos and button

--- app/src/main/java/com/madhadesgames/ebizcard/MainActivity.kt
+++ app/src/main/java/com/madhadesgames/ebizcard/MainActivity.kt
@@ -9,17 +9,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.madhadesgames.ebizcard.ui.theme.EbizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,24 +52,63 @@ fun CreateBizCard() {
             backgroundColor = Color.White,
             elevation = 4.dp
         ) {
-            Surface(modifier = Modifier
-                .size(150.dp)
-                .padding(5.dp),
-            shape = CircleShape,
-            border = BorderStroke(0.5.dp, Color.LightGray),
-                elevation = 4.dp,
-                color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)) {
+           Column(modifier = Modifier.height(300.dp),
+               verticalArrangement = Arrangement.Top,
+               horizontalAlignment = Alignment.CenterHorizontally) {
+               CreateImageProfile()
+               Divider()
+               CreateInfo()
+               Button(onClick = { /*TODO*/ }) {
+                   Text(text = "Portfolio")
+               }
 
-                Image(painter = painterResource(id = R.drawable.roger), contentDescription = "Profile picture",
-                modifier = Modifier.size(135.dp),
-                contentScale = ContentScale.Crop)
-
-            }
+           }
 
         }
 
     }
 
+}
+
+@Composable
+private fun CreateInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            text = "Nicolas Gauthé",
+            fontSize = 24.sp,
+            color = Color.Blue
+        )
+        Text(
+            text = "Junior Android Developer",
+            modifier = Modifier.padding(3.dp)
+        )
+        Text(
+            text = "@MadHadesGames",
+            modifier = Modifier.padding(3.dp)
+        )
+    }
+}
+
+@Composable
+private fun CreateImageProfile() {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.roger),
+            contentDescription = "Profile picture",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
