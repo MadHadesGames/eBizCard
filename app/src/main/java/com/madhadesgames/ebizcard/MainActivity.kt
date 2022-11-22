304eb5556c5915157073c452322ac553d54f6662
MadHadesGames <madhadesgames@gmail.com>
2022-11-22 08:48:38 +0100

    Adding Content function to hide/unhide projects on Portfolio

--- app/src/main/java/com/madhadesgames/ebizcard/MainActivity.kt
+++ app/src/main/java/com/madhadesgames/ebizcard/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.madhadesgames.ebizcard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,6 +47,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+    val buttonClickedState = remember{ mutableStateOf(false) }
     Surface(modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
@@ -58,8 +65,16 @@ fun CreateBizCard() {
                CreateImageProfile()
                Divider()
                CreateInfo()
-               Button(onClick = { /*TODO*/ }) {
-                   Text(text = "Portfolio")
+               Button(onClick = {
+                   buttonClickedState.value = !buttonClickedState.value
+               }) {
+                   Text(text = "Portfolio",
+                   style = MaterialTheme.typography.button)
+               }
+               if (buttonClickedState.value) {
+                   Content()
+               } else {
+                   Box {}
                }
 
            }
@@ -68,6 +83,33 @@ fun CreateBizCard() {
 
     }
 
+}
+@Preview
+@Composable
+fun Content() {
+    Box(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .padding(5.dp)) {
+        Surface(modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        shape = RoundedCornerShape(6.dp),
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            Portfolio(data = listOf("Projet 1", "Projet 2", "Projet 3"))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn{
+        items(data) {
+            item -> Text(item)
+        }
+    }
 }
 
 @Composable
